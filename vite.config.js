1dc22ae3352ca22f57e85fb2f5ac49b39ea362d3
adelabdulkrime-arch <adelabdulkrime@gmail.com>
2026-07-14 10:19:27 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react' // أو أي إضافات موجودة لديك بالفعل
 
 export default defineConfig({
-  base: '/baha-lite/', // أضف هذا السطر تماماً
-  plugins: [react()],
+  // تحديد المسار الصحيح للموقع على GitHub Pages
+  base: '/baha-lite/', 
 })
